e84fd6f8eb48f11104059e83dc6aef8ef7fc5f2c
Nukepayload2 <1939357182@qq.com>
2019-01-16 22:55:58 +0800

    Upgraded credit card fraud detection to 0.9

--- samples/visualbasic/getting-started/BinaryClassification_CreditCardFraudDetection/CreditCardFraudDetection.Common/Extensions.vb
+++ samples/visualbasic/getting-started/BinaryClassification_CreditCardFraudDetection/CreditCardFraudDetection.Common/Extensions.vb
@@ -1,32 +1,29 @@
 ﻿Imports Microsoft.ML
 Imports Microsoft.ML.Core.Data
-Imports Microsoft.ML.Runtime.Data
-Imports System
-Imports System.Collections.Generic
-Imports System.IO
+Imports Microsoft.ML.Data
+Imports System.Runtime.CompilerServices
 
 Namespace CreditCardFraudDetection.Common
     Public Module ConsoleExtensions
 
-        <System.Runtime.CompilerServices.Extension>
-        Public Sub ToConsole(ByVal result As MultiClassClassifierEvaluator.Result)
+        <Extension>
+        Public Sub ToConsole(result As MultiClassClassifierMetrics)
             Console.WriteLine($"Acuracy macro: {result.AccuracyMacro}")
             Console.WriteLine($"Acuracy micro: {result.AccuracyMicro}")
             Console.WriteLine($"Log loss: {result.LogLoss}")
             Console.WriteLine($"Log loss reduction: {result.LogLossReduction}")
             Console.WriteLine($"Per class log loss: ")
             Dim count As Integer = 0
-            For Each logLossClass In result.PerClassLogLoss
-                Console.WriteLine(vbTab & $" [{count + 1}]: {logLossClass}")
-                count += 1
-            Next logLossClass
+            For i = 0 To result.PerClassLogLoss.Length - 1
+                Dim logLossClass = result.PerClassLogLoss(i)
+                Console.WriteLine(vbTab & $" [{i}]: {logLossClass}")
+            Next
             Console.WriteLine($"Top K: {result.TopK}")
             Console.WriteLine($"Top K accuracy: {result.TopKAccuracy}")
-
         End Sub
 
-        <System.Runtime.CompilerServices.Extension>
-        Public Sub ToConsole(ByVal result As BinaryClassifierEvaluator.CalibratedResult)
+        <Extension>
+        Public Sub ToConsole(result As CalibratedBinaryClassificationMetrics)
             Console.WriteLine($"Area under ROC curve: {result.Auc}")
             Console.WriteLine($"Area under the precision/recall curve: {result.Auprc}")
             Console.WriteLine($"Entropy: {result.Entropy}")
@@ -39,8 +36,8 @@ Namespace CreditCardFraudDetection.Common
 
         End Sub
 
-        <System.Runtime.CompilerServices.Extension>
-        Public Sub ToConsole(ByVal result As RegressionEvaluator.Result)
+        <Extension>
+        Public Sub ToConsole(result As RegressionMetrics)
             Console.WriteLine($"L1: {result.L1}")
             Console.WriteLine($"L2: {result.L2}")
             Console.WriteLine($"Loss function: {result.LossFn}")
@@ -48,8 +45,8 @@ Namespace CreditCardFraudDetection.Common
             Console.WriteLine($"R scuared: {result.RSquared}")
         End Sub
 
-        <System.Runtime.CompilerServices.Extension>
-        Public Function ReadModel(ByVal mlContext As MLContext, ByVal modelLocation As String) As ITransformer
+        <Extension>
+        Public Function ReadModel(mlContext As MLContext, modelLocation As String) As ITransformer
             Dim model As ITransformer
             Using file = System.IO.File.OpenRead(modelLocation)
                 model = mlContext.Model.Load(file)
