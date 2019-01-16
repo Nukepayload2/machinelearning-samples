35dd1da20def271d06b72eaba6ed66047d926333
Nukepayload2 <1939357182@qq.com>
2019-01-16 22:58:37 +0800

    Upgraded SentimentAnalysis to 0.9

--- samples/visualbasic/getting-started/BinaryClassification_SentimentAnalysis/SentimentAnalysis/SentimentAnalysisConsoleApp/DataStructures/SentimentPrediction.vb
+++ samples/visualbasic/getting-started/BinaryClassification_SentimentAnalysis/SentimentAnalysis/SentimentAnalysisConsoleApp/DataStructures/SentimentPrediction.vb
@@ -1,16 +1,16 @@
-﻿Imports Microsoft.ML.Runtime.Api
+﻿Imports Microsoft.ML.Data
 
 Namespace SentimentAnalysisConsoleApp.DataStructures
-	Public Class SentimentPrediction
-		' ColumnName attribute is used to change the column name from
-		' its default value, which is the name of the field.
-		<ColumnName("PredictedLabel")>
-		Public Property Prediction() As Boolean
+    Public Class SentimentPrediction
+        ' ColumnName attribute is used to change the column name from
+        ' its default value, which is the name of the field.
+        <ColumnName("PredictedLabel")>
+        Public Property Prediction As Boolean
 
-		' No need to specify ColumnName attribute, because the field
-		' name "Probability" is the column name we want.
-		Public Property Probability() As Single
+        ' No need to specify ColumnName attribute, because the field
+        ' name "Probability" is the column name we want.
+        Public Property Probability As Single
 
-		Public Property Score() As Single
-	End Class
+        Public Property Score As Single
+    End Class
 End Namespace
