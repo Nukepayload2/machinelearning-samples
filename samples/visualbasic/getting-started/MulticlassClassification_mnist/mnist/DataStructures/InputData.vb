cf4bd8c2186597e2cb0aefb91731c9a50235493e
Nukepayload2 <1939357182@qq.com>
2019-02-05 11:46:11 +0800

    Converted MulticlassClassification_Iris

---
+++ samples/visualbasic/getting-started/MulticlassClassification_mnist/mnist/DataStructures/InputData.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.ML.Data
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+
+Namespace mnist.DataStructures
+	Friend Class InputData
+		<ColumnName("PixelValues")>
+		<VectorType(64)>
+		Public PixelValues() As Single
+	End Class
+End Namespace
---
+++ samples/visualbasic/getting-started/MulticlassClassification_mnist/mnist/DataStructures/OutPutNum.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.ML.Data
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+
+Namespace mnist.DataStructures
+	Friend Class OutPutNum
+
+		<ColumnName("Score")>
+		Public Score() As Single
+	End Class
+End Namespace
