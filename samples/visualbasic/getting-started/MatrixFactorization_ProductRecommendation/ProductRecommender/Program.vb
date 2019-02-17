d2010b5d9a2704cc2671bcda07c4c5517311901a
Nukepayload2 <1939357182@qq.com>
2019-02-17 16:27:24 +0800

    Upgraded ProductRecommender 0.10

--- samples/visualbasic/getting-started/MatrixFactorization_ProductRecommendation/ProductRecommender/Program.vb
+++ samples/visualbasic/getting-started/MatrixFactorization_ProductRecommendation/ProductRecommender/Program.vb
@@ -1,60 +1,62 @@
 ﻿Imports Microsoft.ML
+Imports Microsoft.ML.Core.Data
 Imports Microsoft.ML.Data
 Imports Microsoft.ML.Trainers
 
 Namespace ProductRecommender
-    Friend Class Program
+    Friend Module Program
         '1. Do remember to replace amazon0302.txt with dataset from https://snap.stanford.edu/data/amazon0302.html
         '2. Replace column names with ProductID and CoPurchaseProductID. It should look like this:
         '   ProductID	ProductID_Copurchased
         '   0	1
         '   0  2
-        Private Shared TrainingDataLocation As String = $"./Data/Amazon0302.txt"
+        Private TrainingDataLocation As String = $"./Data/Amazon0302.txt"
 
-        Private Shared ModelPath As String = $"./Model/model.zip"
+        Private ModelPath As String = $"./Model/model.zip"
 
-        Shared Sub Main(ByVal args() As String)
+        Sub Main(ByVal args() As String)
             'STEP 1: Create MLContext to be shared across the model creation workflow objects 
-            Dim ctx = New MLContext()
+            Dim mlContext As New MLContext()
 
-            'STEP 2: Create a reader by defining the schema for reading the product co-purchase dataset
+            'STEP 2: Read the trained data using TextLoader by defining the schema for reading the product co-purchase dataset
             '        Do remember to replace amazon0302.txt with dataset from https://snap.stanford.edu/data/amazon0302.html
-            Dim reader = ctx.Data.CreateTextReader({
-                    New TextLoader.Column("Label", DataKind.R4, 0),
-                    New TextLoader.Column("ProductID", DataKind.U4, New TextLoader.Range() {New TextLoader.Range(0)}, New KeyRange(0, 262110)),
-                    New TextLoader.Column("CoPurchaseProductID", DataKind.U4, New TextLoader.Range() {New TextLoader.Range(1)}, New KeyRange(0, 262110))
-                },
-                separatorChar:=vbTab,
-                hasHeader:=True
-            )
+            Dim traindata = mlContext.Data.ReadFromTextFile(path:=TrainingDataLocation, columns:={
+                New TextLoader.Column(DefaultColumnNames.Label, DataKind.R4, 0),
+                New TextLoader.Column(name:=NameOf(ProductEntry.ProductID), type:=DataKind.U4, source:=New TextLoader.Range() {New TextLoader.Range(0)}, keyCount:=New KeyCount(262111)),
+                New TextLoader.Column(name:=NameOf(ProductEntry.CoPurchaseProductID), type:=DataKind.U4, source:=New TextLoader.Range() {New TextLoader.Range(1)}, keyCount:=New KeyCount(262111))
+            }, hasHeader:=True, separatorChar:=vbTab)
 
-            'STEP 3: Read the training data which will be used to train the movie recommendation model
-            Dim traindata = reader.Read(New MultiFileSource(TrainingDataLocation))
+            'STEP 3: Your data is already encoded so all you need to do is specify options for MatrxiFactorizationTrainer with a few extra hyperparameters
+            '        LossFunction, Alpa, Lambda and a few others like K and C as shown below and call the trainer. 
+            Dim options As New MatrixFactorizationTrainer.Options()
+            options.MatrixColumnIndexColumnName = NameOf(ProductEntry.ProductID)
+            options.MatrixRowIndexColumnName = NameOf(ProductEntry.CoPurchaseProductID)
+            options.LabelColumnName = DefaultColumnNames.Label
+            options.LossFunction = MatrixFactorizationTrainer.LossFunctionType.SquareLossOneClass
+            options.Alpha = 0.01
+            options.Lambda = 0.025
+            ' For better results use the following parameters
+            'options.K = 100;
+            'options.C = 0.00001;
 
-
-            'STEP 4: Your data is already encoded so all you need to do is call the MatrixFactorization Trainer with a few extra hyperparameters
-            '        LossFunction, Alpa, Lambda and a few others like K and C as shown below. 
-            Dim est = ctx.Recommendation().Trainers.MatrixFactorization("ProductID", "CoPurchaseProductID", labelColumn:="Label", advancedSettings:=Sub(s)
-                                                                                                                                                        s.LossFunction = MatrixFactorizationTrainer.LossFunctionType.SquareLossOneClass
-                                                                                                                                                        s.Alpha = 0.01
-                                                                                                                                                        s.Lambda = 0.025
-                                                                                                                                                        ' For better results use the following parameters
-                                                                                                                                                        's.K = 100;
-                                                                                                                                                        's.C = 0.00001;
-                                                                                                                                                    End Sub)
+            'Step 4: Call the MatrixFactorization trainer by passing options.
+            Dim est = mlContext.Recommendation().Trainers.MatrixFactorization(options)
 
             'STEP 5: Train the model fitting to the DataSet
             'Please add Amazon0302.txt dataset from https://snap.stanford.edu/data/amazon0302.html to Data folder if FileNotFoundException is thrown.
-            Dim model = est.Fit(traindata)
-
+            Dim model As ITransformer = est.Fit(traindata)
 
             'STEP 6: Create prediction engine and predict the score for Product 63 being co-purchased with Product 3.
             '        The higher the score the higher the probability for this particular productID being co-purchased 
-            Dim predictionengine = model.CreatePredictionEngine(Of ProductEntry, Copurchase_prediction)(ctx)
+            Dim predictionengine = model.CreatePredictionEngine(Of ProductEntry, Copurchase_prediction)(mlContext)
             Dim prediction = predictionengine.Predict(New ProductEntry() With {
                 .ProductID = 3,
                 .CoPurchaseProductID = 63
             })
+
+            Console.WriteLine(vbLf & " For ProductID = 3 and  CoPurchaseProductID = 63 the predicted score is " & Math.Round(prediction.Score, 1))
+            Console.WriteLine("=============== End of process, hit any key to finish ===============")
+            Console.ReadKey()
         End Sub
 
         Public Class Copurchase_prediction
@@ -62,11 +64,12 @@ Namespace ProductRecommender
         End Class
 
         Public Class ProductEntry
-            <KeyType(Contiguous:=True, Count:=262111, Min:=0)>
+            <KeyType(Count:=262111)>
             Public Property ProductID() As UInteger
 
-            <KeyType(Contiguous:=True, Count:=262111, Min:=0)>
+            <KeyType(Count:=262111)>
             Public Property CoPurchaseProductID() As UInteger
         End Class
-    End Class
+    End Module
+
 End Namespace
