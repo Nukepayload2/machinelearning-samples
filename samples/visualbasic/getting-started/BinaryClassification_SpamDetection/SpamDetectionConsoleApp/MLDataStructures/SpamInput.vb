a2e2094331691b5226fa1893cfb31d54c4f279d5
Nukepayload2 <1939357182@qq.com>
2019-02-17 15:37:12 +0800

    Upgraded SpamDetectionConsoleApp to 0.10

--- samples/visualbasic/getting-started/BinaryClassification_SpamDetection/SpamDetectionConsoleApp/MLDataStructures/SpamInput.vb
+++ samples/visualbasic/getting-started/BinaryClassification_SpamDetection/SpamDetectionConsoleApp/MLDataStructures/SpamInput.vb
@@ -1,6 +1,10 @@
-﻿Namespace SpamDetectionConsoleApp.MLDataStructures
-	Friend Class SpamInput
-		Public Property Label() As String
-		Public Property Message() As String
-	End Class
+﻿Imports Microsoft.ML.Data
+
+Namespace SpamDetectionConsoleApp.MLDataStructures
+    Friend Class SpamInput
+        <LoadColumn(0)>
+        Public Property Label As String
+        <LoadColumn(1)>
+        Public Property Message As String
+    End Class
 End Namespace
--- samples/visualbasic/getting-started/BinaryClassification_SpamDetection/SpamDetectionConsoleApp/MLDataStructures/SpamPrediction.vb
+++ samples/visualbasic/getting-started/BinaryClassification_SpamDetection/SpamDetectionConsoleApp/MLDataStructures/SpamPrediction.vb
@@ -1,11 +1,11 @@
-﻿Imports Microsoft.ML.Runtime.Api
+﻿Imports Microsoft.ML.Data
 
 Namespace SpamDetectionConsoleApp.MLDataStructures
-	Friend Class SpamPrediction
-		<ColumnName("PredictedLabel")>
-		Public Property isSpam() As Boolean
+    Friend Class SpamPrediction
+        <ColumnName("PredictedLabel")>
+        Public Property isSpam As Boolean
 
-		Public Property Score() As Single
-		Public Property Probability() As Single
-	End Class
+        Public Property Score As Single
+        Public Property Probability As Single
+    End Class
 End Namespace
