2fd0c4d3cd672fc8131928fb4681a5d2896804c6
Nukepayload2 <1939357182@qq.com>
2019-03-02 14:56:13 +0800

    Updated SentimentAnalysisConsoleApp

--- samples/visualbasic/getting-started/BinaryClassification_SentimentAnalysis/SentimentAnalysis/SentimentAnalysisConsoleApp/Program.vb
+++ samples/visualbasic/getting-started/BinaryClassification_SentimentAnalysis/SentimentAnalysis/SentimentAnalysisConsoleApp/Program.vb
@@ -1,193 +1,114 @@
-﻿Option Infer On
-
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Threading.Tasks
-Imports System.IO
-
-' Requires following NuGet packages
-' NuGet package -> Microsoft.Extensions.Configuration
-' NuGet package -> Microsoft.Extensions.Configuration.Json
-Imports Microsoft.Extensions.Configuration
-
-Imports Microsoft.ML
-Imports Microsoft.ML.Transforms.Conversions
+﻿Imports System.IO
 Imports Microsoft.ML.Core.Data
-
-Imports Common
-Imports GitHubLabeler.DataStructures
+Imports Microsoft.ML
 Imports Microsoft.ML.Data
 
-Namespace GitHubLabeler
-	Friend Module Program
-		Private ReadOnly Property AppPath() As String
-			Get
-				Return Path.GetDirectoryName(Environment.GetCommandLineArgs()(0))
-			End Get
-		End Property
+Imports SentimentAnalysisConsoleApp.DataStructures
+Imports Common
+Imports Microsoft.Data.DataView
 
-		Private BaseDatasetsRelativePath As String = "../../../../Data"
-		Private DataSetRelativePath As String = $"{BaseDatasetsRelativePath}/corefx-issues-train.tsv"
-		Private DataSetLocation As String = GetAbsolutePath(DataSetRelativePath)
+Namespace SentimentAnalysisConsoleApp
+    Friend Module Program
+        Private ReadOnly Property AppPath As String
+            Get
+                Return Path.GetDirectoryName(Environment.GetCommandLineArgs()(0))
+            End Get
+        End Property
 
-		Private BaseModelsRelativePath As String = "../../../../MLModels"
-		Private ModelRelativePath As String = $"{BaseModelsRelativePath}/GitHubLabelerModel.zip"
-		Private ModelPath As String = GetAbsolutePath(ModelRelativePath)
+        Private ReadOnly BaseDatasetsRelativePath As String = "../../../../Data"
+        Private ReadOnly TrainDataRelativePath As String = $"{BaseDatasetsRelativePath}/wikipedia-detox-250-line-data.tsv"
+        Private ReadOnly TestDataRelativePath As String = $"{BaseDatasetsRelativePath}/wikipedia-detox-250-line-test.tsv"
 
+        Private TrainDataPath As String = GetAbsolutePath(TrainDataRelativePath)
+        Private TestDataPath As String = GetAbsolutePath(TestDataRelativePath)
 
-		Public Enum MyTrainerStrategy 
-			SdcaMultiClassTrainer = 1
-			OVAAveragedPerceptronTrainer = 2
-		End Enum
+        Private ReadOnly BaseModelsRelativePath As String = "../../../../MLModels"
+        Private ReadOnly ModelRelativePath As String = $"{BaseModelsRelativePath}/SentimentModel.zip"
 
-		Public Property Configuration() As IConfiguration
-		Public Async Function Main(ByVal args() As String) As Task
-			SetupAppConfiguration()
+        Private ModelPath As String = GetAbsolutePath(ModelRelativePath)
 
-			'1. ChainedBuilderExtensions and Train the model
-			BuildAndTrainModel(DataSetLocation, ModelPath, MyTrainerStrategy.SdcaMultiClassTrainer)
+        Sub Main(args() As String)
+            'Create MLContext to be shared across the model creation workflow objects 
+            'Set a random seed for repeatable/deterministic results across multiple trainings.
+            Dim mlContext = New MLContext(seed:=1)
 
-			'2. Try/test to predict a label for a single hard-coded Issue
-			TestSingleLabelPrediction(ModelPath)
+            ' Create, Train, Evaluate and Save a model
+            BuildTrainEvaluateAndSaveModel(mlContext)
+            Common.ConsoleHelper.ConsoleWriteHeader("=============== End of training process ===============")
 
-			'3. Predict Issue Labels and apply into a real GitHub repo
-			' (Comment the next line if no real access to GitHub repo) 
-			Await PredictLabelsAndUpdateGitHub(ModelPath)
+            ' Make a single test prediction loding the model from .ZIP file
+            TestSinglePrediction(mlContext)
 
-			Common.ConsoleHelper.ConsolePressAnyKey()
-		End Function
+            Common.ConsoleHelper.ConsoleWriteHeader("=============== End of process, hit any key to finish ===============")
+            Console.ReadKey()
 
-		Public Sub BuildAndTrainModel(ByVal DataSetLocation As String, ByVal ModelPath As String, ByVal selectedStrategy As MyTrainerStrategy)
-			' Create MLContext to be shared across the model creation workflow objects 
-			' Set a random seed for repeatable/deterministic results across multiple trainings.
-			Dim mlContext = New MLContext(seed:= 1)
+        End Sub
 
-			' STEP 1: Common data loading configuration
-			Dim trainingDataView = mlContext.Data.ReadFromTextFile(Of GitHubIssue)(DataSetLocation, hasHeader:= True, separatorChar:=ControlChars.Tab, supportSparse:= False)
+        Private Function BuildTrainEvaluateAndSaveModel(mlContext As MLContext) As ITransformer
+            ' STEP 1: Common data loading configuration
+            Dim trainingDataView As IDataView = mlContext.Data.ReadFromTextFile(Of SentimentIssue)(TrainDataPath, hasHeader:=True)
+            Dim testDataView As IDataView = mlContext.Data.ReadFromTextFile(Of SentimentIssue)(TestDataPath, hasHeader:=True)
 
-			' STEP 2: Common data process configuration with pipeline data transformations
-			Dim dataProcessPipeline = mlContext.Transforms.Conversion.MapValueToKey(outputColumnName:= DefaultColumnNames.Label,inputColumnName:=NameOf(GitHubIssue.Area)).Append(mlContext.Transforms.Text.FeaturizeText(outputColumnName:= "TitleFeaturized",inputColumnName:=NameOf(GitHubIssue.Title))).Append(mlContext.Transforms.Text.FeaturizeText(outputColumnName:= "DescriptionFeaturized", inputColumnName:= NameOf(GitHubIssue.Description))).Append(mlContext.Transforms.Concatenate(outputColumnName:=DefaultColumnNames.Features, "TitleFeaturized", "DescriptionFeaturized")).AppendCacheCheckpoint(mlContext)
-							' Use in-memory cache for small/medium datasets to lower training time. 
-							' Do NOT use it (remove .AppendCacheCheckpoint()) when handling very large datasets.
+            ' STEP 2: Common data process configuration with pipeline data transformations          
+            Dim dataProcessPipeline = mlContext.Transforms.Text.FeaturizeText(outputColumnName:=DefaultColumnNames.Features, inputColumnName:=NameOf(SentimentIssue.Text))
 
-			' (OPTIONAL) Peek data (such as 2 records) in training DataView after applying the ProcessPipeline's transformations into "Features" 
-			Common.ConsoleHelper.PeekDataViewInConsole(mlContext, trainingDataView, dataProcessPipeline, 2)
+            ' (OPTIONAL) Peek data (such as 2 records) in training DataView after applying the ProcessPipeline's transformations into "Features" 
+            ConsoleHelper.PeekDataViewInConsole(mlContext, trainingDataView, dataProcessPipeline, 2)
+            ConsoleHelper.PeekVectorColumnDataInConsole(mlContext, DefaultColumnNames.Features, trainingDataView, dataProcessPipeline, 1)
 
-			' STEP 3: Create the selected training algorithm/trainer
-			Dim trainer As IEstimator(Of ITransformer) = Nothing
-			Select Case selectedStrategy
-				Case MyTrainerStrategy.SdcaMultiClassTrainer
-					 trainer = mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(DefaultColumnNames.Label, DefaultColumnNames.Features)
-				Case MyTrainerStrategy.OVAAveragedPerceptronTrainer
-					' Create a binary classification trainer.
-					Dim averagedPerceptronBinaryTrainer = mlContext.BinaryClassification.Trainers.AveragedPerceptron(DefaultColumnNames.Label, DefaultColumnNames.Features, numIterations:= 10)
-					' Compose an OVA (One-Versus-All) trainer with the BinaryTrainer.
-					' In this strategy, a binary classification algorithm is used to train one classifier for each class, "
-					' which distinguishes that class from all other classes. Prediction is then performed by running these binary classifiers, "
-					' and choosing the prediction with the highest confidence score.
-					trainer = mlContext.MulticlassClassification.Trainers.OneVersusAll(averagedPerceptronBinaryTrainer)
+            ' STEP 3: Set the training algorithm, then create and config the modelBuilder                            
+            Dim trainer = mlContext.BinaryClassification.Trainers.FastTree(labelColumn:=DefaultColumnNames.Label, featureColumn:=DefaultColumnNames.Features)
+            Dim trainingPipeline = dataProcessPipeline.Append(trainer)
 
-					Exit Select
-				Case Else
-			End Select
+            ' STEP 4: Train the model fitting to the DataSet
+            Console.WriteLine("=============== Training the model ===============")
+            Dim trainedModel As ITransformer = trainingPipeline.Fit(trainingDataView)
 
-			'Set the trainer/algorithm and map label to value (original readable state)
-			Dim trainingPipeline = dataProcessPipeline.Append(trainer).Append(mlContext.Transforms.Conversion.MapKeyToValue(DefaultColumnNames.PredictedLabel))
+            ' STEP 5: Evaluate the model and show accuracy stats
+            Console.WriteLine("===== Evaluating Model's accuracy with Test data =====")
+            Dim predictions = trainedModel.Transform(testDataView)
+            Dim metrics = mlContext.BinaryClassification.Evaluate(data:=predictions, label:=DefaultColumnNames.Label, score:=DefaultColumnNames.Score)
 
-			' STEP 4: Cross-Validate with single dataset (since we don't have two datasets, one for training and for evaluate)
-			' in order to evaluate and get the model's accuracy metrics
+            ConsoleHelper.PrintBinaryClassificationMetrics(trainer.ToString(), metrics)
 
-			Console.WriteLine("=============== Cross-validating to get model's accuracy metrics ===============")
+            ' STEP 6: Save/persist the trained model to a .ZIP file
 
-			'Measure cross-validation time
-			Dim watchCrossValTime = System.Diagnostics.Stopwatch.StartNew()
+            Using fs = New FileStream(ModelPath, FileMode.Create, FileAccess.Write, FileShare.Write)
+                mlContext.Model.Save(trainedModel, fs)
+            End Using
 
-			Dim crossValidationResults = mlContext.MulticlassClassification.CrossValidate(data:=trainingDataView, estimator:=trainingPipeline, numFolds:= 6, labelColumn:=DefaultColumnNames.Label)
+            Console.WriteLine("The model is saved to {0}", ModelPath)
 
-			'Stop measuring time
-			watchCrossValTime.Stop()
-			Dim elapsedMs As Long = watchCrossValTime.ElapsedMilliseconds
-			Console.WriteLine($"Time Cross-Validating: {elapsedMs} miliSecs")
+            Return trainedModel
+        End Function
 
-			ConsoleHelper.PrintMulticlassClassificationFoldsAverageMetrics(DirectCast(trainer, Object).ToString(), crossValidationResults)
+        ' (OPTIONAL) Try/test a single prediction by loding the model from the file, first.
+        Private Sub TestSinglePrediction(mlContext As MLContext)
+            Dim sampleStatement As SentimentIssue = New SentimentIssue With {.Text = "This is a very rude movie"}
 
-			' STEP 5: Train the model fitting to the DataSet
-			Console.WriteLine("=============== Training the model ===============")
+            Dim trainedModel As ITransformer
+            Using stream = New FileStream(ModelPath, FileMode.Open, FileAccess.Read, FileShare.Read)
+                trainedModel = mlContext.Model.Load(stream)
+            End Using
 
-			'Measure training time
-			Dim watch = System.Diagnostics.Stopwatch.StartNew()
+            ' Create prediction engine related to the loaded trained model
+            Dim predEngine = trainedModel.CreatePredictionEngine(Of SentimentIssue, SentimentPrediction)(mlContext)
 
-			Dim trainedModel = trainingPipeline.Fit(trainingDataView)
+            'Score
+            Dim resultprediction = predEngine.Predict(sampleStatement)
 
-			'Stop measuring time
-			watch.Stop()
-			Dim elapsedCrossValMs As Long = watch.ElapsedMilliseconds
+            Console.WriteLine($"=============== Single Prediction  ===============")
+            Console.WriteLine($"Text: {sampleStatement.Text} | Prediction: {(If(Convert.ToBoolean(resultprediction.Prediction), "Negative", "Nice"))} sentiment | Probability: {resultprediction.Probability} ")
+            Console.WriteLine($"==================================================")
+        End Sub
 
-			Console.WriteLine($"Time Training the model: {elapsedCrossValMs} miliSecs")
+        Public Function GetAbsolutePath(relativePath As String) As String
+            Dim _dataRoot As New FileInfo(GetType(Program).Assembly.Location)
+            Dim assemblyFolderPath As String = _dataRoot.Directory.FullName
 
-			' (OPTIONAL) Try/test a single prediction with the "just-trained model" (Before saving the model)
-			Dim issue As New GitHubIssue() With {
-				.ID = "Any-ID",
-				.Title = "WebSockets communication is slow in my machine",
-				.Description = "The WebSockets communication used under the covers by SignalR looks like is going slow in my development machine.."
-			}
-			' Create prediction engine related to the loaded trained model
-			Dim predEngine = trainedModel.CreatePredictionEngine(Of GitHubIssue, GitHubIssuePrediction)(mlContext)
-			'Score
-			Dim prediction = predEngine.Predict(issue)
-			Console.WriteLine($"=============== Single Prediction just-trained-model - Result: {prediction.Area} ===============")
-			'
+            Dim fullPath As String = Path.Combine(assemblyFolderPath, relativePath)
 
-			' STEP 6: Save/persist the trained model to a .ZIP file
-			Console.WriteLine("=============== Saving the model to a file ===============")
-			Using fs = New FileStream(ModelPath, FileMode.Create, FileAccess.Write, FileShare.Write)
-				mlContext.Model.Save(trainedModel, fs)
-			End Using
-
-			Common.ConsoleHelper.ConsoleWriteHeader("Training process finalized")
-		End Sub
-
-		Private Sub TestSingleLabelPrediction(ByVal modelFilePathName As String)
-			Dim labeler = New Labeler(modelPath:= ModelPath)
-			labeler.TestPredictionForSingleIssue()
-		End Sub
-
-		Private Async Function PredictLabelsAndUpdateGitHub(ByVal ModelPath As String) As Task
-			Console.WriteLine(".............Retrieving Issues from GITHUB repo, predicting label/s and assigning predicted label/s......")
-
-			Dim token = Configuration("GitHubToken")
-			Dim repoOwner = Configuration("GitHubRepoOwner") 'IMPORTANT: This can be a GitHub User or a GitHub Organization
-			Dim repoName = Configuration("GitHubRepoName")
-
-			If String.IsNullOrEmpty(token) OrElse token Is "YOUR - GUID - GITHUB - TOKEN" OrElse String.IsNullOrEmpty(repoOwner) OrElse repoOwner Is "YOUR-REPO-USER-OWNER-OR-ORGANIZATION" OrElse String.IsNullOrEmpty(repoName) OrElse repoName Is "YOUR-REPO-SINGLE-NAME" Then
-				Console.Error.WriteLine()
-				Console.Error.WriteLine("Error: please configure the credentials in the appsettings.json file")
-				Console.ReadLine()
-				Return
-			End If
-
-			'This "Labeler" class could be used in a different End-User application (Web app, other console app, desktop app, etc.) 
-			Dim labeler = New Labeler(ModelPath, repoOwner, repoName, token)
-
-			Await labeler.LabelAllNewIssuesInGitHubRepo()
-
-			Console.WriteLine("Labeling completed")
-			Console.ReadLine()
-		End Function
-
-		Private Sub SetupAppConfiguration()
-			Dim builder = (New ConfigurationBuilder()).SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json")
-
-			Configuration = builder.Build()
-		End Sub
-
-		Public Function GetAbsolutePath(ByVal relativePath As String) As String
-			Dim _dataRoot As New FileInfo(GetType(Program).Assembly.Location)
-			Dim assemblyFolderPath As String = _dataRoot.Directory.FullName
-
-			Dim fullPath As String = Path.Combine(assemblyFolderPath, relativePath)
-
-			Return fullPath
-		End Function
-	End Module
+            Return fullPath
+        End Function
+    End Module
 End Namespace
