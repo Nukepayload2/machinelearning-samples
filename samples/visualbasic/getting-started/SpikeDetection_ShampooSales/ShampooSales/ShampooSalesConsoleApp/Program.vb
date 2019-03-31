482efaf0f7078ca6a65a28743dcc30c4e7ea3300
Nukepayload2 <1939357182@qq.com>
2019-03-31 11:25:15 +0800

    Updated ShampooSales

--- samples/visualbasic/getting-started/SpikeDetection_ShampooSales/ShampooSales/ShampooSalesConsoleApp/Program.vb
+++ samples/visualbasic/getting-started/SpikeDetection_ShampooSales/ShampooSales/ShampooSalesConsoleApp/Program.vb
@@ -16,20 +16,34 @@ Namespace ShampooSales
 		Private ModelPath As String = GetAbsolutePath(ModelRelativePath)
 
 		Sub Main()
-            ' Create MLContext to be shared across the model creation workflow objects 
-            Dim mlcontext As New MLContext
+			' Create MLContext to be shared across the model creation workflow objects 
+			Dim mlcontext As MLContext = New MLContext
 
-            'assign the Number of records in dataset file to cosntant variable
-            Const size As Integer = 36
+			'assign the Number of records in dataset file to cosntant variable
+			Const size As Integer = 36
 
 			'STEP 1: Common data loading configuration
-			Dim dataView As IDataView = mlcontext.Data.LoadFromTextFile(Of ShampooSalesData)(path:= DatasetPath, hasHeader:=True, separatorChar:=","c)
+			Dim dataView As IDataView = mlcontext.Data.LoadFromTextFile(Of ShampooSalesData)(path:= DatasetPath, hasHeader:= True, separatorChar:= ","c)
+
+			'To detech temporay changes in the pattern
+			DetectSpike(mlcontext,size,dataView)
+
+			'To detect persistent change in the pattern
+			DetectChangepoint(mlcontext, size, dataView)
+
+			Console.WriteLine("=============== End of process, hit any key to finish ===============")
+
+			Console.ReadLine()
+		End Sub
+
+		Private Sub DetectSpike(mlcontext As MLContext, size As Integer, dataView As IDataView)
+		   Console.WriteLine("Detect temporary changes in pattern")
 
 			'STEP 2: Set the training algorithm    
 			Dim trainingPipeLine = mlcontext.Transforms.DetectIidSpike(outputColumnName:= NameOf(ShampooSalesPrediction.Prediction), inputColumnName:= NameOf(ShampooSalesData.numSales),confidence:= 95, pvalueHistoryLength:= size \ 4)
 
 			'STEP 3:Train the model by fitting the dataview
-			Console.WriteLine("=============== Training the model ===============")
+			Console.WriteLine("=============== Training the model using Spike Detection algorithm ===============")
 			Dim trainedModel As ITransformer = trainingPipeLine.Fit(dataView)
 			Console.WriteLine("=============== End of training process ===============")
 
@@ -47,9 +61,34 @@ Namespace ShampooSales
 				Console.ResetColor()
 			Next p
 			Console.WriteLine("")
-			Console.WriteLine("=============== End of process, hit any key to finish ===============")
+		End Sub
 
-			Console.ReadLine()
+		Private Sub DetectChangepoint(mlcontext As MLContext, size As Integer, dataView As IDataView)
+		  Console.WriteLine("Detect Persistent changes in pattern")
+
+		  'STEP 2: Set the training algorithm    
+		  Dim trainingPipeLine = mlcontext.Transforms.DetectIidChangePoint(outputColumnName:= NameOf(ShampooSalesPrediction.Prediction), inputColumnName:= NameOf(ShampooSalesData.numSales), confidence:= 95, changeHistoryLength:= size \ 4)
+
+		  'STEP 3:Train the model by fitting the dataview
+		  Console.WriteLine("=============== Training the model Using Change Point Detection Algorithm===============")
+		  Dim trainedModel As ITransformer = trainingPipeLine.Fit(dataView)
+		  Console.WriteLine("=============== End of training process ===============")
+
+		  'Apply data transformation to create predictions.
+		  Dim transformedData As IDataView = trainedModel.Transform(dataView)
+		  Dim predictions = mlcontext.Data.CreateEnumerable(Of ShampooSalesPrediction)(transformedData, reuseRowObject:= False)
+
+		  Console.WriteLine($"{NameOf(ShampooSalesPrediction.Prediction)} column obtained post-transformation.")
+		  Console.WriteLine("Alert" & vbTab & "Score" & vbTab & "P-Value" & vbTab & "Martingale value")
+
+		  For Each p In predictions
+			 If p.Prediction(0) = 1 Then
+				 Console.WriteLine("{0}" & vbTab & "{1:0.00}" & vbTab & "{2:0.00}" & vbTab & "{3:0.00}  <-- alert is on, predicted changepoint", p.Prediction(0), p.Prediction(1), p.Prediction(2), p.Prediction(3))
+			 Else
+				 Console.WriteLine("{0}" & vbTab & "{1:0.00}" & vbTab & "{2:0.00}" & vbTab & "{3:0.00}", p.Prediction(0), p.Prediction(1), p.Prediction(2), p.Prediction(3))
+			 End If
+		  Next p
+			Console.WriteLine("")
 		End Sub
 
 		Public Function GetAbsolutePath(relativePath As String) As String
