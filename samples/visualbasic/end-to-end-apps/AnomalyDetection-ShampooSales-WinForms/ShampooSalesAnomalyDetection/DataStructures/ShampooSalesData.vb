1c19a8f8c19927406b1e4eb35842a3cad59d12bc
Nukepayload2 <1939357182@qq.com>
2019-03-31 11:44:01 +0800

    Converted ShampooSalesAnomalyDetection

---
+++ samples/visualbasic/end-to-end-apps/AnomalyDetection-ShampooSales-WinForms/ShampooSalesAnomalyDetection/DataStructures/ShampooSalesData.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.ML.Data
+
+Namespace ShampooSalesAnomalyDetection
+	Public Class ShampooSalesData
+		<LoadColumn(0)>
+		Public Month As String
+
+		<LoadColumn(1)>
+		Public numSales As Single
+	End Class
+End Namespace
---
+++ samples/visualbasic/end-to-end-apps/AnomalyDetection-ShampooSales-WinForms/ShampooSalesAnomalyDetection/DataStructures/ShampooSalesPrediction.vb
@@ -0,0 +1,9 @@
+﻿Imports Microsoft.ML.Data
+
+Namespace ShampooSalesAnomalyDetection
+	Friend Class ShampooSalesPrediction
+		' Vector to hold Alert, Score, and P-Value values
+		<VectorType(3)>
+		Public Property Prediction As Double()
+	End Class
+End Namespace
