80fcb3a6bdc1f8bf9676564dbfb1e2aa5692f07f
Nukepayload2 <1939357182@qq.com>
2019-05-12 13:39:07 +0800

    Fixed build error in auto ml

--- samples/visualbasic/common/AutoML/ProgressHandlers.vb
+++ samples/visualbasic/common/AutoML/ProgressHandlers.vb
@@ -11,23 +11,23 @@ Namespace Common
 
 		Private _iterationIndex As Integer
 
-		Public Sub Report(iterationResult As RunDetail(Of BinaryClassificationMetrics))
-'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
-'ORIGINAL LINE: if (_iterationIndex++ == 0)
-			If _iterationIndex = 0 Then
-				_iterationIndex += 1
-				ConsoleHelper.PrintBinaryClassificationMetricsHeader()
-			Else
-				_iterationIndex += 1
-			End If
+        Public Sub Report(iterationResult As RunDetail(Of BinaryClassificationMetrics)) Implements IProgress(Of RunDetail(Of BinaryClassificationMetrics)).Report
+            'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+            'ORIGINAL LINE: if (_iterationIndex++ == 0)
+            If _iterationIndex = 0 Then
+                _iterationIndex += 1
+                ConsoleHelper.PrintBinaryClassificationMetricsHeader()
+            Else
+                _iterationIndex += 1
+            End If
 
-			If iterationResult.Exception IsNot Nothing Then
-				ConsoleHelper.PrintIterationException(iterationResult.Exception)
-			Else
-				ConsoleHelper.PrintIterationMetrics(_iterationIndex, iterationResult.TrainerName, iterationResult.ValidationMetrics, iterationResult.RuntimeInSeconds)
-			End If
-		End Sub
-	End Class
+            If iterationResult.Exception IsNot Nothing Then
+                ConsoleHelper.PrintIterationException(iterationResult.Exception)
+            Else
+                ConsoleHelper.PrintIterationMetrics(_iterationIndex, iterationResult.TrainerName, iterationResult.ValidationMetrics, iterationResult.RuntimeInSeconds)
+            End If
+        End Sub
+    End Class
 
 	''' <summary>
 	''' Progress handler that AutoML will invoke after each model it produces and evaluates.
@@ -37,47 +37,47 @@ Namespace Common
 
 		Private _iterationIndex As Integer
 
-		Public Sub Report(iterationResult As RunDetail(Of MulticlassClassificationMetrics))
-'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
-'ORIGINAL LINE: if (_iterationIndex++ == 0)
-			If _iterationIndex = 0 Then
-				_iterationIndex += 1
-				ConsoleHelper.PrintMulticlassClassificationMetricsHeader()
-			Else
-				_iterationIndex += 1
-			End If
+        Public Sub Report(iterationResult As RunDetail(Of MulticlassClassificationMetrics)) Implements IProgress(Of RunDetail(Of MulticlassClassificationMetrics)).Report
+            'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+            'ORIGINAL LINE: if (_iterationIndex++ == 0)
+            If _iterationIndex = 0 Then
+                _iterationIndex += 1
+                ConsoleHelper.PrintMulticlassClassificationMetricsHeader()
+            Else
+                _iterationIndex += 1
+            End If
 
-			If iterationResult.Exception IsNot Nothing Then
-				ConsoleHelper.PrintIterationException(iterationResult.Exception)
-			Else
-				ConsoleHelper.PrintIterationMetrics(_iterationIndex, iterationResult.TrainerName, iterationResult.ValidationMetrics, iterationResult.RuntimeInSeconds)
-			End If
-		End Sub
-	End Class
+            If iterationResult.Exception IsNot Nothing Then
+                ConsoleHelper.PrintIterationException(iterationResult.Exception)
+            Else
+                ConsoleHelper.PrintIterationMetrics(_iterationIndex, iterationResult.TrainerName, iterationResult.ValidationMetrics, iterationResult.RuntimeInSeconds)
+            End If
+        End Sub
+    End Class
 
 	''' <summary>
 	''' Progress handler that AutoML will invoke after each model it produces and evaluates.
 	''' </summary>
 	Public Class RegressionExperimentProgressHandler
-		Implements IProgress(Of RunDetail(Of RegressionMetrics))
+        Implements IProgress(Of RunDetail(Of RegressionMetrics))
 
-		Private _iterationIndex As Integer
+        Private _iterationIndex As Integer
 
-		Public Sub Report(iterationResult As RunDetail(Of RegressionMetrics))
-'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
-'ORIGINAL LINE: if (_iterationIndex++ == 0)
-			If _iterationIndex = 0 Then
-				_iterationIndex += 1
-				ConsoleHelper.PrintRegressionMetricsHeader()
-			Else
-				_iterationIndex += 1
-			End If
+        Public Sub Report(iterationResult As RunDetail(Of RegressionMetrics)) Implements IProgress(Of RunDetail(Of RegressionMetrics)).Report
+            'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+            'ORIGINAL LINE: if (_iterationIndex++ == 0)
+            If _iterationIndex = 0 Then
+                _iterationIndex += 1
+                ConsoleHelper.PrintRegressionMetricsHeader()
+            Else
+                _iterationIndex += 1
+            End If
 
-			If iterationResult.Exception IsNot Nothing Then
-				ConsoleHelper.PrintIterationException(iterationResult.Exception)
-			Else
-				ConsoleHelper.PrintIterationMetrics(_iterationIndex, iterationResult.TrainerName, iterationResult.ValidationMetrics, iterationResult.RuntimeInSeconds)
-			End If
-		End Sub
-	End Class
+            If iterationResult.Exception IsNot Nothing Then
+                ConsoleHelper.PrintIterationException(iterationResult.Exception)
+            Else
+                ConsoleHelper.PrintIterationMetrics(_iterationIndex, iterationResult.TrainerName, iterationResult.ValidationMetrics, iterationResult.RuntimeInSeconds)
+            End If
+        End Sub
+    End Class
 End Namespace
